b0c678509a27ada1fa37926e5ccf13506426457b
ALİ KARA <77415061+aliikara@users.noreply.github.com>
2021-02-07 15:21:58 +0300

    9. Gün Ödev 1 (6 Şubat)

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -13,6 +13,54 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car,RecapContext> , ICarDal
     {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (RecapContext recapContext = new RecapContext())
+            {
+                var result = from c in recapContext.Cars
+                             join b in recapContext.Brands on c.BrandId equals b.BrandId
+                             join co in recapContext.Colors on c.ColorId equals co.ColorId
+                             select new CarDetailDto
+                             {
+                                 CarId = c.CarId,
+                                 BrandName = b.BrandName,
+                                 ColorName = co.ColorName,
+                                 DailyPrice = c.DailyPrice
+                             };
+                return result.ToList();
+
+
+            }
+
+            /*using (RecapContext context = new RecapContext())
+            {
+                var result = context.Cars
+                    .Include(c => c.Color)
+                    .Include(c => c.Brand)
+                    .Select(c => new CarDetailDto
+                    {
+                        BrandName = c.Brand.BrandName == null ? "Marka yok" : c.Brand.BrandName,
+                        ColorName = c.Color.ColorName == null ? "Renk yok" : c.Color.ColorName,
+                        DailyPrice = c.DailyPrice
+                    }).ToList();
+            }
+
+            using (RecapContext context = new RecapContext())
+            {
+                return (from car in context.Cars
+                        join color in context.Colors on car.ColorId equals color.ColorId into temp
+                        from t in temp.DefaultIfEmpty()
+                        join brand in context.Brands on car.BrandId equals brand.BrandId into temp2
+                        from t2 in temp2.DefaultIfEmpty()
+                        select new CarDetailDto
+                        {
+                            BrandName = t2.BrandName == null ? "Marka yok" : t2.BrandName,
+                            ColorName = t.ColorName == null ? "Renk yok" : t.ColorName,
+                            DailyPrice = car.DailyPrice
+                        }).ToList();
+            }*/
+        }
+
         /*public void Add(Car entity)
         {
             using (RecapContext recapContext=new RecapContext())
@@ -58,53 +106,5 @@ namespace DataAccess.Concrete.EntityFramework
                 recapContext.SaveChanges();
             }
         }*/
-
-        public List<CarDetailDto> GetCarDetails()
-        {
-            using (RecapContext recapContext=new RecapContext())
-            {
-                var result = from c in recapContext.Cars
-                             join b in recapContext.Brands on c.BrandId equals b.BrandId
-                             join co in recapContext.Colors on c.ColorId equals co.ColorId
-                             select new CarDetailDto
-                             {
-                                 CarId = c.CarId,
-                                 BrandName = b.BrandName,
-                                 ColorName = co.ColorName,
-                                 DailyPrice = c.DailyPrice
-                             };
-                return result.ToList();
-
-
-            }
-
-            /*using (RecapContext context = new RecapContext())
-            {
-                var result = context.Cars
-                    .Include(c => c.Color)
-                    .Include(c => c.Brand)
-                    .Select(c => new CarDetailDto
-                    {
-                        BrandName = c.Brand.BrandName == null ? "Marka yok" : c.Brand.BrandName,
-                        ColorName = c.Color.ColorName == null ? "Renk yok" : c.Color.ColorName,
-                        DailyPrice = c.DailyPrice
-                    }).ToList();
-            }
-
-            using (RecapContext context = new RecapContext())
-            {
-                return (from car in context.Cars
-                        join color in context.Colors on car.ColorId equals color.ColorId into temp
-                        from t in temp.DefaultIfEmpty()
-                        join brand in context.Brands on car.BrandId equals brand.BrandId into temp2
-                        from t2 in temp2.DefaultIfEmpty()
-                        select new CarDetailDto
-                        {
-                            BrandName = t2.BrandName == null ? "Marka yok" : t2.BrandName,
-                            ColorName = t.ColorName == null ? "Renk yok" : t.ColorName,
-                            DailyPrice = car.DailyPrice
-                        }).ToList();
-            }*/
-        }
     }
 }
